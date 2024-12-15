d5ab29e76f6012babc92e5a7d867b0dcc12ff537
XVushubaloX <battleboy434@gmail.com>
2024-12-15 11:44:37 +0100

    Initial commit

---
+++ src/main/java/flyinmyeye/interium/InteriumClient.java
@@ -0,0 +1,11 @@
+package flyinmyeye.interium;
+
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InteriumClient  implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/flyinmyeye/interium/InteriumDataGenerator.java
@@ -0,0 +1,11 @@
+package flyinmyeye.interium;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class InteriumDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
