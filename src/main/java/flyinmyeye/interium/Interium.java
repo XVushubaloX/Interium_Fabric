03cafac665ddd7a44dd90509d45f117f94f052ab
XVushubaloX <battleboy434@gmail.com>
2024-12-16 07:57:10 +0100

    base items added

--- src/main/java/flyinmyeye/interium/Interium.java
+++ src/main/java/flyinmyeye/interium/Interium.java
@@ -1,5 +1,6 @@
 package flyinmyeye.interium;
 
+import flyinmyeye.interium.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,7 +12,7 @@ public class Interium implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/flyinmyeye/interium/item/ModItems.java
@@ -0,0 +1,39 @@
+package flyinmyeye.interium.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import flyinmyeye.interium.Interium;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item BIOIRON = registerItem("bioiron", new Item(new Item.Settings()));
+    public static final Item CHLOROPHYLL = registerItem("chlorophyll", new Item(new Item.Settings()));
+    public static final Item PURE_CHLOROPHYLL = registerItem("pure_chlorophyll", new Item(new Item.Settings()));
+    public static final Item CONTAMINATED_CHLOROPHYLL = registerItem("contaminated_chlorophyll", new Item(new Item.Settings()));
+    public static final Item BIOSTEEL = registerItem("biosteel", new Item(new Item.Settings()));
+    public static final Item MITOCHONDRIA = registerItem("mitochondria", new Item(new Item.Settings()));
+    public static final Item MORTAR_AND_PISTLE = registerItem("mortar_and_pestle", new Item(new Item.Settings()));
+    public static final Item SPORE = registerItem("spore", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Interium.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        Interium.LOGGER.info("Registering Mod Items for " + Interium.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(BIOIRON);
+            entries.add(CHLOROPHYLL);
+            entries.add(PURE_CHLOROPHYLL);
+            entries.add(CONTAMINATED_CHLOROPHYLL);
+            entries.add(BIOSTEEL);
+            entries.add(MITOCHONDRIA);
+            entries.add(MORTAR_AND_PISTLE);
+            entries.add(SPORE);
+        });
+    }
+}
